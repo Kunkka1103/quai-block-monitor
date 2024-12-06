41cd92901c5989bb8555d9b39ee1ecc18b954138
kunkka <kunkka@Kunkkas-MacBook-Pro-office.local>
2024-12-06 15:06:00 +0800

    add prod-env

--- main.go
+++ main.go
@@ -17,83 +17,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/push"
 )
 
-func main() {
-	// 定义命令行参数
-	dsn := flag.String("dsn", "", "PostgreSQL DSN")
-	pushURL := flag.String("push-url", "", "Pushgateway URL")
-	interval := flag.Duration("interval", time.Minute, "Interval between queries")
-	jobName := flag.String("job", "", "job name for Pushgateway")
-	env := flag.String("env", "", "Environment: dev/prod")
-	rpcURL := flag.String("rpc-url", "", "RPC URL to get block number")
-	hostname := flag.String("hostname", "", "Hostname of the server")
-	officialURL := flag.String("official-url", "", "Official URL of the quai")
-
-	flag.Parse()
-
-	if *dsn == "" || *pushURL == "" || *rpcURL == "" {
-		log.Fatal("DSN, push-url, and rpc-url must be provided")
-	}
-
-	log.Printf("Starting program with DSN: %s, Pushgateway URL: %s, Interval: %s, Job Name: %s, RPC URL: %s", *dsn, *pushURL, *interval, *jobName, *rpcURL)
-
-	// 连接到数据库
-	db, err := sql.Open("postgres", *dsn)
-	if err != nil {
-		log.Fatalf("Failed to open database connection: %v", err)
-	}
-	defer db.Close()
-	log.Println("Successfully connected to the database")
-
-	// 定义 Prometheus Gauge
-	blockHeightGauge := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "quai_block_height",
-		Help: "The block height of quai from different sources",
-	})
-
-	// 定期查询数据库和RPC并推送数据到 Pushgateway
-	for {
-		log.Println("Querying the database for the maximum block height...")
-		var maxHeight int
-		err := db.QueryRow("SELECT MAX(number) FROM blocks").Scan(&maxHeight)
-		if err != nil {
-			log.Printf("Failed to execute query: %v", err)
-			blockHeightGauge.Set(-1) // 查询失败时，设置为特殊值
-			pushMetrics(*pushURL, *jobName, *env, "db", "", blockHeightGauge)
-		} else {
-			log.Printf("Query successful, maximum block height: %d", maxHeight)
-			blockHeightGauge.Set(float64(maxHeight))
-			pushMetrics(*pushURL, *jobName, *env, "db", "", blockHeightGauge)
-		}
-
-		log.Println("Querying the RPC for the current block height...")
-		rpcHeight, err := fetchBlockHeightFromRPC(*rpcURL)
-		if err != nil {
-			log.Printf("Failed to fetch block height from RPC: %v", err)
-			blockHeightGauge.Set(-1) // 查询失败时，设置为特殊值
-			pushMetrics(*pushURL, *jobName, *env, "rpc", *hostname, blockHeightGauge)
-		} else {
-			log.Printf("RPC query successful, current block height: %d", rpcHeight)
-			blockHeightGauge.Set(float64(rpcHeight))
-			pushMetrics(*pushURL, *jobName, *env, "rpc", *hostname, blockHeightGauge)
-		}
-
-		log.Println("Querying the official for the current block height...")
-		officialHeight, err := fetchBlockHeightFromRPC(*officialURL)
-		if err != nil {
-			log.Printf("Failed to fetch block height from official: %v", err)
-			blockHeightGauge.Set(-1) // 查询失败时，设置为特殊值
-			pushMetrics(*pushURL, *jobName, "", "official", "", blockHeightGauge)
-		} else {
-			log.Printf("official query successful, current block height: %d", officialHeight)
-			blockHeightGauge.Set(float64(officialHeight))
-			pushMetrics(*pushURL, *jobName, "", "official", "", blockHeightGauge)
-		}
-
-		log.Printf("Sleeping for %s before the next query", *interval)
-		time.Sleep(*interval)
-	}
-}
-
 // fetchBlockHeightFromRPC 从 RPC 获取块高度
 func fetchBlockHeightFromRPC(rpcURL string) (int64, error) {
 	payload := `{
@@ -102,65 +25,213 @@ func fetchBlockHeightFromRPC(rpcURL string) (int64, error) {
 		"params": [],
 		"id": 1
 	}`
-
 	req, err := http.NewRequest("POST", rpcURL, strings.NewReader(payload))
 	if err != nil {
 		return 0, fmt.Errorf("failed to create request: %w", err)
 	}
-
 	req.Header.Set("Content-Type", "application/json")
-
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		return 0, fmt.Errorf("failed to execute request: %w", err)
 	}
 	defer resp.Body.Close()
-
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return 0, fmt.Errorf("failed to read response body: %w", err)
 	}
-
 	var rpcResponse struct {
 		Result string `json:"result"`
 	}
-
 	if err := json.Unmarshal(body, &rpcResponse); err != nil {
 		return 0, fmt.Errorf("failed to unmarshal response: %w", err)
 	}
-
-	// 将 16 进制的 result 转换为 10 进制的整数
 	height, err := strconv.ParseInt(rpcResponse.Result, 0, 64)
 	if err != nil {
 		return 0, fmt.Errorf("failed to parse block height: %w", err)
 	}
-
 	return height, nil
 }
 
 // pushMetrics 使用 Pushgateway 推送数据
 func pushMetrics(pushURL, jobName, env, source, hostname string, gauge prometheus.Gauge) {
 	log.Printf("Pushing metrics to Pushgateway at %s with job name %s, source %s...", pushURL, jobName, source)
-
-	// 创建 Pushgateway pusher 对象
 	pusher := push.New(pushURL, jobName).
 		Grouping("source", source).
+		Grouping("env", env).
 		Collector(gauge)
-
-	// 如果提供了 hostname，则添加 hostname 标签
 	if hostname != "" {
 		pusher.Grouping("hostname", hostname)
 	}
-
-	// 如果提供了 env，则添加 env 标签
-	if env != "" {
-		pusher.Grouping("env", env)
-	}
-	// 推送数据
 	if err := pusher.Push(); err != nil {
 		log.Printf("Could not push to Pushgateway: %v", err)
 	} else {
 		log.Println("Pushed metrics successfully")
+	}
+}
+
+func main() {
+	// 命令行参数
+	pushURL := flag.String("push-url", "", "Pushgateway URL")
+	interval := flag.Duration("interval", time.Minute, "Interval between queries")
+	jobName := flag.String("job", "", "Job name for Pushgateway")
+
+	// dev 环境参数
+	devDsn := flag.String("dev-dsn", "", "PostgreSQL DSN for dev environment")
+	devRpcUrls := flag.String("dev-rpc-urls", "", "Comma-separated list of RPC URLs for dev environment, with hostname (url:hostname)")
+
+	// prod 环境参数
+	prodDsn := flag.String("prod-dsn", "", "PostgreSQL DSN for prod environment")
+	prodRpcUrls := flag.String("prod-rpc-urls", "", "Comma-separated list of RPC URLs for prod environment, with hostname (url:hostname)")
+
+	// 官方 URL
+	officialURL := flag.String("official-url", "", "Official URL of the block height")
+
+	flag.Parse()
+
+	if *pushURL == "" || *officialURL == "" {
+		log.Fatal("Pushgateway URL and official URL must be provided")
+	}
+
+	// dev 和 prod 环境的节点列表
+	devNodeURLs := strings.Split(*devRpcUrls, ",")
+	prodNodeURLs := strings.Split(*prodRpcUrls, ",")
+
+	if *devDsn == "" && *prodDsn == "" {
+		log.Fatal("At least one of dev-dsn or prod-dsn must be provided")
+	}
+
+	// 连接数据库
+	var devDb, prodDb *sql.DB
+	var err error
+
+	if *devDsn != "" {
+		devDb, err = sql.Open("postgres", *devDsn)
+		if err != nil {
+			log.Fatalf("[dev] Failed to connect to database: %v", err)
+		}
+		defer devDb.Close()
+	}
+
+	if *prodDsn != "" {
+		prodDb, err = sql.Open("postgres", *prodDsn)
+		if err != nil {
+			log.Fatalf("[prod] Failed to connect to database: %v", err)
+		}
+		defer prodDb.Close()
+	}
+
+	for {
+		// 处理 dev 环境
+		if devDb != nil {
+			log.Println("Processing dev environment")
+
+			// 查询数据库块高度
+			var maxHeight int
+			err = devDb.QueryRow("SELECT MAX(number) FROM blocks").Scan(&maxHeight)
+			if err != nil {
+				log.Printf("[dev] Failed to query database: %v", err)
+				maxHeight = -1
+			} else {
+				log.Printf("[dev] Max block height from database: %d", maxHeight)
+			}
+
+			// 推送数据库高度
+			blockHeightGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+				Name: "quai_block_height",
+				Help: "The block height of quai from different sources",
+			})
+			blockHeightGauge.Set(float64(maxHeight))
+			pushMetrics(*pushURL, *jobName, "dev", "db", "", blockHeightGauge)
+
+			// 查询每个 RPC 节点的高度
+			for _, rpcEntry := range devNodeURLs {
+				splitEntry := strings.Split(strings.TrimSpace(rpcEntry), ":")
+				if len(splitEntry) != 2 {
+					log.Printf("[dev] Invalid RPC entry format: %s", rpcEntry)
+					continue
+				}
+				rpcURL := splitEntry[0]
+				hostname := splitEntry[1]
+
+				rpcHeight, err := fetchBlockHeightFromRPC(rpcURL)
+				if err != nil {
+					log.Printf("[dev] Failed to query RPC (%s): %v", rpcURL, err)
+					rpcHeight = -1
+				} else {
+					log.Printf("[dev] Block height from RPC (%s): %d", rpcURL, rpcHeight)
+				}
+
+				// 推送 RPC 节点的高度
+				blockHeightGauge.Set(float64(rpcHeight))
+				pushMetrics(*pushURL, *jobName, "dev", "rpc", hostname, blockHeightGauge)
+			}
+		}
+
+		// 处理 prod 环境
+		if prodDb != nil {
+			log.Println("Processing prod environment")
+
+			// 查询数据库块高度
+			var maxHeight int
+			err = prodDb.QueryRow("SELECT MAX(number) FROM blocks").Scan(&maxHeight)
+			if err != nil {
+				log.Printf("[prod] Failed to query database: %v", err)
+				maxHeight = -1
+			} else {
+				log.Printf("[prod] Max block height from database: %d", maxHeight)
+			}
+
+			// 推送数据库高度
+			blockHeightGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+				Name: "quai_block_height",
+				Help: "The block height of quai from different sources",
+			})
+			blockHeightGauge.Set(float64(maxHeight))
+			pushMetrics(*pushURL, *jobName, "prod", "db", "", blockHeightGauge)
+
+			// 查询每个 RPC 节点的高度
+			for _, rpcEntry := range prodNodeURLs {
+				splitEntry := strings.Split(strings.TrimSpace(rpcEntry), ":")
+				if len(splitEntry) != 2 {
+					log.Printf("[prod] Invalid RPC entry format: %s", rpcEntry)
+					continue
+				}
+				rpcURL := splitEntry[0]
+				hostname := splitEntry[1]
+
+				rpcHeight, err := fetchBlockHeightFromRPC(rpcURL)
+				if err != nil {
+					log.Printf("[prod] Failed to query RPC (%s): %v", rpcURL, err)
+					rpcHeight = -1
+				} else {
+					log.Printf("[prod] Block height from RPC (%s): %d", rpcURL, rpcHeight)
+				}
+
+				// 推送 RPC 节点的高度
+				blockHeightGauge.Set(float64(rpcHeight))
+				pushMetrics(*pushURL, *jobName, "prod", "rpc", hostname, blockHeightGauge)
+			}
+		}
+
+		// 查询官方 URL 的块高度
+		officialHeight, err := fetchBlockHeightFromRPC(*officialURL)
+		if err != nil {
+			log.Printf("Failed to query official URL: %v", err)
+			officialHeight = -1
+		} else {
+			log.Printf("Block height from official URL: %d", officialHeight)
+		}
+
+		// 推送官方 URL 的高度
+		blockHeightGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "quai_block_height",
+			Help: "The block height of quai from different sources",
+		})
+		blockHeightGauge.Set(float64(officialHeight))
+		pushMetrics(*pushURL, *jobName, "official", *officialURL, "",blockHeightGauge)
+
+		log.Printf("Sleeping for %s before the next query", *interval)
+		time.Sleep(*interval)
 	}
 }
